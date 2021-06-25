88b827d9e3731e3a87bd40bd0b2d6fef2ea70af5
pdmxdd <pdmxdd@gmail.com>
2021-06-25 10:25:33 -0500

    added ternary example

---
+++ src/com/company/Ternary.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Ternary {
+
+    public static void main(String[] args) {
+        int someNumber = 5;
+        System.out.println(someNumber == 5 ? "The number was 5" : "The number was NOT 5");
+    }
+}
